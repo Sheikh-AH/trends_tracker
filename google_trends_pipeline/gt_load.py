7e53e27b0024f53a243aa4fdede927b9ce0662bc
xmashalx <Mashalchr@gmail.com>
2026-02-04 11:22:00 +0000

    add docstrings, change variable names, change order of import

--- google_trends_pipeline/gt_load.py
+++ google_trends_pipeline/gt_load.py
@@ -1,9 +1,11 @@
 """This module loads keyword search volume data into a PostgreSQL database."""
-import psycopg2
+
 import os
+import psycopg2
 
 
 def get_db_connection() -> psycopg2.extensions.connection:
+    """Establish and return a connection to the PostgreSQL database."""
     return psycopg2.connect(
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT"),
